53b7a934aaa93606d38a92a5dc4dacf6a179e164
Abdoul Aziz <salgo397@gmail.com>
2026-02-23 12:52:20 +0000

    Activation des logs pour debug

--- backend/settings.py
+++ backend/settings.py
@@ -223,3 +223,25 @@ else:
     # Relaxed security for development
     X_FRAME_OPTIONS = 'SAMEORIGIN'
 
+# Logging configuration
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    },
+}
+
